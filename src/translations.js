5a4d45f7717f1c7792ffda548bfcdc8797b9b045
scaramat <Mateo.Scarafia@externals.adidas.com>
2020-01-25 17:40:01 +0100

    impr

---
+++ src/translations.js
@@ -0,0 +1,16 @@
+export const español = {
+  login: 'Login',
+  registrate: 'Registrate',
+  ultimasNoticias: 'Últimas noticias',
+  slogan: 'Tú eres el periodista',
+  buscar: 'Buscar',
+  buscarArticulo: 'Buscar artículo',
+  olvideMiContraseña: 'Olvidé mi contraseña',
+  noTienesCuenta: '¿No tienes cuenta? Registrate',
+  ingreseSuEmail: 'Ingrese su email',
+  recuperarContraseña: 'Recuperar contraseña',
+};
+
+export const ingles = {};
+
+export const portugues = {};
