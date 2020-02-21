79bfc1fa3253b9a3b6d9f17ef25b59b3243ab90f
scaramat <Mateo.Scarafia@externals.adidas.com>
2020-02-21 14:53:33 +0100

    impr

--- src/features/home/ErrorLanding.js
+++ src/features/home/ErrorLanding.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import * as VALUES from '../../constants';
 
 export default class ErrorLanding extends Component {
-  static propTypes = {
 
+  static propTypes = {};
+
+  goHome = () => {
+    window.location.replace(VALUES.FRONTEND_URL + 'feed/main');
   };
 
   render() {
@@ -10,7 +14,11 @@ export default class ErrorLanding extends Component {
       <div className="home-error-landing">
         <img alt="edit" width="50" src={require('../../images/logo.png')} />
         <p className="p-subtitle">Lo sentimos, ocurrió un error.</p>
+        <button onClick={() => this.goHome()} className="p-subtitle-bis">
+        Refrescar
+        </button>
       </div>
     );
   }
+  
 }
