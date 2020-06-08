9674bb29961c6a6a155d16f8364fc15ba034bcfb
Mateo Scarafia <32779338+mateoscarafia@users.noreply.github.com>
2020-06-08 11:29:28 +0200

    Update TextEditor.js

--- src/features/home/TextEditor.js
+++ src/features/home/TextEditor.js
@@ -147,7 +147,7 @@ export class TextEditor extends Component {
         NotificationManager.info('El archivo no es una imagen/video');
         document.getElementById('button-post-article').style.display = 'inline';
         document.getElementById('spinner-button-post-article').style.display = 'none';
-      } else if (this.state.file && this.state.file.size > 50000000) {
+      } else if (this.state.file && this.state.file.size > 100000000) {
         NotificationManager.info('Lo sentimos, el archivo es muy grande');
         document.getElementById('button-post-article').style.display = 'inline';
         document.getElementById('spinner-button-post-article').style.display = 'none';
