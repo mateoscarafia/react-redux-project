78d754ef785905f6ccf7b72f9ec7823eab64e445
mateocognizant <mateo.scarafia.ext@bayer.com>
2020-06-24 11:08:57 +0200

    impr

--- src/features/home/Wewoordi.js
+++ src/features/home/Wewoordi.js
@@ -35,8 +35,8 @@ export class Wewoordi extends Component {
           />
           <br/><br/>
           <br/>
-          <p id='demo'>Woordi es una red social de periodismo y opinion. Woordi es una plataforma digital que permite a sus usuarios publicar contenido estructurado bajo el concepto de noticia. Es decir, el esquema de sus comunicados responde al de un periódico digital (Articulos/Videos).
-          Buscamos generar una comunidad de redactores independientes que encuentren en Woordi un espacio de libre expresión capaz de catapultar al público sus ideas y percepción de la realidad. Creemos que la concepción de diario digital puede ser adaptado a las tendencias actuales. Hoy en dia es el público quien genera el contenido para si mismo. La tecnologia ha permitido esta descentralización y queremos hacer uso de ella.</p>
+          <p id='demo'>Woordi es una red social de periodismo y opinión. Woordi es una plataforma digital que permite a sus usuarios publicar contenido estructurado bajo el concepto de noticia. Es decir, el esquema de sus comunicados responde al de un periódico digital (Artículos/Videos).
+          Buscamos generar una comunidad de redactores independientes que encuentren en Woordi un espacio de libre expresión capaz de catapultar al público sus ideas y percepción de la realidad. Creemos que la concepción de diario digital puede ser adaptado a las tendencias actuales. Hoy en dia es el público quien genera el contenido para si mismo, la tecnología ha permitido esta descentralización y queremos hacer uso de ella.</p>
         </div>
       </div>
     );
